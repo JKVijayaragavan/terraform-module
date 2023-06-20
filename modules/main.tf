6c01081d429db2f9efa3910b8f807448fdbb31dd
JKVijayaragavan <vijayaraghavn7@gmail.com>
2023-06-20 18:20:32 +0530

    first commit

---
+++ modules/main.tf
@@ -0,0 +1,18 @@
+resource "google_compute_instance" "jenkin_win" {
+    name = var.name
+    machine_type = var.mechine
+    zone = var.zome
+    boot_disk {
+      initialize_params {
+        image = var.image
+      }
+
+    }
+networknetwork_interface {
+  Network = google_compute_network.vpc_network.name
+}
+}
+#Network
+resource "google_compute_network" "vpc_network" {
+  name = "${var.name}-vpc_network"
+}
