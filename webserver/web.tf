c7d8d327ee752a4edaa3a780f4a0aceb8b6e6639
JKVijayaragavan <100981502+JKVijayaragavan@users.noreply.github.com>
2023-06-20 20:54:47 +0530

    deleted it

--- webserver/web.tf
+++
@@ -1,7 +0,0 @@
-module "webserver" {
-    source = "git::https://github.com/JKVijayaragavan/terraform-module.git"
-    name = "jenkin_win"
-    machine = "f1-micro"
-    image = "windows-cloud/windows-2012-r2"
-    zone = "us-west1-a"
-}
