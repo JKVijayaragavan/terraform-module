f97286764094f9dcb3bf6bc0f8a48396b2589397
JKVijayaragavan <vijayaraghavn7@gmail.com>
2023-06-20 20:43:36 +0530

    update

--- webserver/web.tf
+++ webserver/web.tf
@@ -1,5 +1,5 @@
 module "webserver" {
-    source = "../modules"
+    source = "git::https://github.com/JKVijayaragavan/terraform-module.git"
     name = "jenkin_win"
     machine = "f1-micro"
     image = "windows-cloud/windows-2012-r2"
