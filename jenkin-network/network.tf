5658fdfeb6f08dc618b77b2201c45402997df552
JK Vijayaragavan <vijayaraghavn7@gmail.com>
2023-06-25 09:15:41 +0530

    netework update

--- jenkin-network/network.tf
+++ jenkin-network/network.tf
@@ -9,7 +9,7 @@ resource "google_compute_subnetwork" "subnet1" {
   name = var.sunbetname
   ip_cidr_range = "10.0.1.0/24"
   region = "us-west1"
-  network = google_compute_network.vpcnetwork.id
+  network = google_compute_network.vpcnetwork.name
   
 }
 
